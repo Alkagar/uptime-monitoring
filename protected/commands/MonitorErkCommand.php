d3b99f9fe702b8099e26c1224285d7e19df6d171
root <root@pelikan.cusi.uj.edu.pl>
2012-09-07 11:24:08 +0200

    rm commands

--- protected/commands/MonitorErkCommand.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class MonitorErkCommand extends CConsoleCommand
-    {
-        public function actionIndex() 
-        { 
-            $erkSuite = new AErkSuite();
-            $erkSuite->runSuite();
-        }
-    }
--- protected/commands/MonitorErkLogCommand.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class MonitorErkLogCommand extends CConsoleCommand
-    {
-        public function actionIndex() 
-        { 
-            $erkSuite = new AErkLogSuite();
-            $erkSuite->runSuite();
-        }
-    }
